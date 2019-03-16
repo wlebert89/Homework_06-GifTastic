f467479fe173210b52eda76e77bc72f30d50aad5
lqdsqd <wesley.lebert@gmail.com>
2019-03-15 19:33:53 -0500

    finished styling and responsiveness

--- README.markdown
+++ README.markdown
@@ -1,9 +1,15 @@
-This is a timed trivia game in which the user answers one question at a time and receives feedback on each
-question immediately after answering. If the user fails to answer the question within the alotted 30 seconds, 
-the question times out and moves on to the next one.
+This is a gif generator site that uses the giphy API.
+I used AJAX to retrieve the gif information, and Javascript with jQuery to dynamically populate the gifs to the page.
+The gifs are loaded as stills by default, but the user can click any or all of them to set them in motion. A second click will revert it back to a still.
+The user also has the ability to add their own search buttons and find gifs related to any keywords they choose.
+I gave it a custom style using CSS, complete with some over-the-top fonts from the Google Font library.
+I also made the site responsive using media queries.
 
-I used Javascript and jQuery to code the game logic and user interactivity, and designed the look and feel of
-the game using custom CSS.
+Currently, when a user adds a search button, I have it set up so that the newly added button will become the left-most button.
+I did this so that I could keep the buttons all in one row (the buttons that get pushed to the next row will be accessible via a scroll down).
+By keeping the buttons in one row, I was able to set the gif-display area to a fixed height, which I prefer to a page scroll.
+However, I find that it is not overly obvious that the newly added button appears to the left. I would like to find a way to make this more
+apparent in order to improve the user experience.
 
-I also created 5 abstract images, each pertaining to a particular question, using Cinema 4D and Adobe After Effects, 
-to give the game periodic pops of color - especially because color is the theme of the game!
+Also, I would ideally like to find a way to make the gifs fill the white space inside their container better. Sometimes they line up well,
+but I would prefer them to be more dynamic.
--- javascript/logic.js
+++ javascript/logic.js
@@ -1,4 +1,18 @@
-var keywordArray = ["The Who", "The Beatles", "Radiohead", "Neil Young"];
+var keywordArray = [
+  "Night of the Living Dead", 
+  "Nightmare on Elm Street",
+  "The Shining", 
+  "Susperia", 
+  "Evil Dead",
+  "Rosemary's Baby",
+  "The Silence of The Lambs",
+  "In The Mouth of Madness",
+  "The Mist",
+];
+
+keywordArray.reverse();
+
+$("#gif-display").addClass("hide")
 
 function displayGifInfo(){
 
@@ -12,7 +26,11 @@ function displayGifInfo(){
     console.log(response);
 
     // empties the existing gif-display section before populating it.
+    // I'm choosing to clear the div and then append so the results pertain 
+    // only to the button that was most recently clicked.
     $("#gif-display").empty();
+
+    $("#gif-display").removeClass("hide");
     
     var results = response.data;
 
@@ -28,8 +46,8 @@ function displayGifInfo(){
       gif.addClass("gif");
       gifDiv.append(gif);
       gifDiv.append(pRating);
-      gifDiv.append("<hr>");
-      $("#gif-display").append(gifDiv); //I'm choosing to clear the div and then append so the results load faster.
+      // gifDiv.append("<hr>");
+      $("#gif-display").append(gifDiv);
     }
   });
 }
@@ -42,8 +60,11 @@ function renderButtons(){
     var button = $("<button>");
     button.addClass("gif-button");
     button.attr("data-name", keywordArray[i]);
-    button.text(keywordArray[i]);
-    $("#button-display").append(button);
+    button.text(keywordArray[i].toUpperCase());
+    // I'm prepending the buttons so that the one that gets added shows up first in the list.
+    // I did this in order to keep the buttons on one line so that the gif-display div can be
+    // set to a fixed-height.
+    $("#button-display").prepend(button);
   }
 }
 
@@ -51,11 +72,13 @@ $("#add-button").on("click", function(event){
 
   event.preventDefault();
 
-  var gif = $("#gif-input").val().trim();
+  var gif = $("#gif-input").val().trim().toUpperCase();
 
   keywordArray.push(gif);
 
   renderButtons();
+
+  $("#gif-input").val("");
 });
 
 $(document).on("click", ".gif-button", displayGifInfo);
